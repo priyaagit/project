c89590b0593094a5f6cdf8f85823cb78103a83d8
priyaagit <104397792+priyaagit@users.noreply.github.com>
2022-05-24 20:09:36 -0400

    Update kwitter_page.js

--- kwitter_page.js
+++ kwitter_page.js
@@ -2,13 +2,7 @@
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-      apiKey: "AIzaSyC5kMZ7wvJpLAT9u7Z8YBKV59XjF-aqqRE",
-      authDomain: "project-93-letschat-web-app-1.firebaseapp.com",
-      databaseURL: "https://project-93-letschat-web-app-1-default-rtdb.firebaseio.com",
-      projectId: "project-93-letschat-web-app-1",
-      storageBucket: "project-93-letschat-web-app-1.appspot.com",
-      messagingSenderId: "599747272052",
-      appId: "1:599747272052:web:04fa716f9c059b29902021"
+      
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
